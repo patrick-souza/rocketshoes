4b763bd80beed587bb515a8b692c8cd7fdbeefd2
Patrick souza <patrick.souza96@outlook.com>
2020-01-01 17:39:16 -0300

    config redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import Routes from 'routes';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import Routes from './routes';
 import GlobalStyles from './styles/global';
+
 import Header from 'Components/Header';
+import store from './store';
+
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyles />
-      <Header />
-      <Routes />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalStyles />
+        <Header />
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
