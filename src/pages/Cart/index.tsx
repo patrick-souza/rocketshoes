7df39f85b74ea38a3826956cdaf06f3de85129d3
Patrick souza <patrick.souza96@outlook.com>
2020-01-01 16:23:28 -0300

    cart

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,7 +1,67 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <div />;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Produtos</th>
+            <th>QTD</th>
+            <th>Subtotal</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+                alt="Tenis"
+              />
+            </td>
+            <td>
+              <strong>Tenis muito massa</strong>
+              <span>R$ 190,90</span>
+            </td>
+            <td>
+              <div>
+                <button>
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value="1" />
+                <button>
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$360,00</strong>
+            </td>
+            <td>
+              <button>
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button>Finalizar Pedido</button>
+        <Total>
+          <span>Total</span>
+          <strong>R$1920,22</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
