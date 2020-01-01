fdbd0641b2e465aa07ff24c921383070079dc349
Patrick souza <patrick.souza96@outlook.com>
2020-01-01 15:29:34 -0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Routes from 'routes';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyles from './styles/global';
+import Header from 'Components/Header';
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyles />
-      {/* <Header /> */}
+      <Header />
       <Routes />
     </BrowserRouter>
   );
