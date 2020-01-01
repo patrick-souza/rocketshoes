30cd7913f44323f0b21a22d8626e43c568a596d1
Patrick souza <patrick.souza96@outlook.com>
2020-01-01 16:00:16 -0300

    product list

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
-// import { Container } from './styles';
-
+import { ProductList } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      {[1, 2, 3, 4, 5, 6].map(i => (
+        <li key={i}>
+          <img
+            src="https://static.netshoes.com.br/produtos/tenis-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+            alt="Tenis"
+          />
+          <strong>Tenis muito legal</strong>
+          <span>R$ 190,90</span>
+
+          <button>
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+            <span>Adicionar ao Carrinho</span>
+          </button>
+        </li>
+      ))}
+    </ProductList>
+  );
 }
